0cad4507940e859a05c6679fc62bab17028bbaef
Sebo26 <sebomaduma26@gmail.com>
2024-04-07 08:53:59 -0700

    No 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install -y nginx
 mkdir -p "/data/"
 mkdir -p "/data/web_static/"
 mkdir -p "/data/web_static/releases/"
@@ -9,3 +11,7 @@ touch "/data/web_static/releases/test/index.html"
 ln -s "/data/web_static/current" "/data/web_static/releases/test/"
 chown -R "ubuntu"  "/data/"
 chgrp "ubuntu" "/data/"
+server {
+	location hbnb_static {
+	alias /data/web_static/current/;
+}
