e40c3d95570206c2f8101f5b899cc14704722d9e
Sebo26 <sebomaduna26@gmail.com>
2024-05-22 06:57:41 +0000

    No 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def web_flask():
     return "<p>Hello HBNB!</p>"
+
+if __name__ == "__main__":
+    web_flask.run(host="0.0.0.0", port=5000)
